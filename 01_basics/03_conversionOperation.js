1bda216efe08a1ae053e69aa2fdcdaaacea32561
Ajay Bakoliya <146326621+Ajay-016@users.noreply.github.com>
2025-12-29 16:26:16 +0000

    dataTypes Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "20"
+// console.log(typeof score);
+let numberInScore = Number(score) // type conversion of a string to a number
+// console.log(typeof numberInScore);
+
+
+let str = "20abc"
+let numberInStr = Number(str)
+// console.log(numberInStr);
+
+// "33" => 33
+// "20abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "ajay" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
