9649d952afd5c49fff0a0bd7882fde5ee3f4b83e
Ajay Bakoliya <146326621+Ajay-016@users.noreply.github.com>
2025-12-29 16:00:10 +0000

    Variables and DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14455
+let accountEmail = "av7730748@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState
+// accountId = 2
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+// we don't use var in js because it doesn't follow block-scope.
+// if you just define a variable and doesn't decalare a value of it, js by default takes the value as undefined.
+
+
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // use all JS code as newer version
+let age = 22;
+// number => -(2⁵³ - 1) to 2⁵³ - 1
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => uniqueness
+// object
+
+console.log(typeof "ajay");
+console.log(typeof age);
+console.log(typeof null); // typeof null is object.
+console.log(typeof undefined); // typeof undefined is undefined.
+
+
